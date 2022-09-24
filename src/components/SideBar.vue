280d7531eea68bbc488988583b411083c5595d0a
reynoldadade <reynoldadade@gmail.com>
2022-09-24 11:32:23 +0000

    setup sidebar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,5 +1,13 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="container__sideBar"></div>
+  <div class="container__sideBar">
+    <div class="links">
+      <router-link to="/">Inbox</router-link>
+      <router-link to="/archive">Archive</router-link>
+    </div>
+    <div class="links">
+      <router-link to="/">Logout</router-link>
+    </div>
+  </div>
 </template>
--- src/views/Inbox.vue
+++ src/views/Inbox.vue
@@ -2,6 +2,11 @@
   <div>
     <h1>Inbox</h1>
     <h2>Emails Selected()</h2>
+    <div class="card">
+      <input type="checkbox" class="item" />
+      <button class="item">Mark as red(r)</button>
+      <button class="item">Archive(r)</button>
+    </div>
   </div>
 </template>
 
